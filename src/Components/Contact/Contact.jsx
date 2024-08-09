acde4367777070e616d6f7f90c18f219e942e957
JAISINGH DHANDE <jaidhande3@gmail.com>
2024-08-10 01:43:13 +0530

    completed the contact us section

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -6,33 +6,76 @@ import locationIcon from "../../assets/location-icon.png";
 import "./Contact.css";
 
 const Contact = () => {
+
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "85c6c357-3b25-4c9f-9be2-410e073d3406");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
   return (
-    <div>
-      <div className="contact-left">
-        <div className="send-message">
-          <p>Send us a message</p>
-          <img src={messageIcon}></img>
-        </div>
+    <div className="contact">
+      <div className="contact-col">
+        <h3>
+          Send us a message <img src={messageIcon}></img>
+        </h3>
         <p>
           Feel free to reach out through contact form or find our contact
           information below. Your feedback, questions, and suggestions are
           important to us as we strive to provide exceptional service to our
           university community.
         </p>
-        <div className="send-message">
-          <img src={mailIcon}></img>
-          <p>Contact@GreatStack.dev</p>
-        </div>
-        <div className="send-message">
-          <img src={callIocn}></img>
-          <p>+1 123-456-7890</p>
-        </div>
-        <div className="send-message">
-          <img src={locationIcon}></img>
-          <p>77 Massachusetts Ave, Cambridge MA 02139, United States</p>
-        </div>
+        <ul>
+          <li>
+            <img src={mailIcon}></img>Contact@GreatStack.dev
+          </li>
+          <li>
+            <img src={callIocn}></img>+1 123-456-7890
+          </li>
+          <li>
+            <img src={locationIcon}></img>77 Massachusetts Ave, Cambridge MA
+            02139, United States
+          </li>
+        </ul>
       </div>
-      <div className="contact-right"></div>
+      <div className="contact-col">
+        <form onSubmit={onSubmit}>
+          <label>Your Name</label>
+          <input type="text" name="name" placeholder="Enter your name" required></input>
+          <label>Phone Number</label>
+          <input type="number" name="phone" placeholder="Enter your number" required></input>
+          <label>Your Email</label>
+          <input type="email" name="email" placeholder="Enter your email" required></input>
+          <label>Write your message here</label>
+          <textarea
+            type="text"
+            placeholder="Enter your message"
+            name="message"
+            required
+          ></textarea>
+          <button type="submit"  className="btn dark-btn">Submit</button>
+        </form>
+        <span>{result}</span>
+      </div>
     </div>
   );
 };
