674aa304462e764e29c1f7eadb4c96fe78d0ed53
JAISINGH DHANDE <jaidhande3@gmail.com>
2024-08-11 13:56:34 +0530

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import About from "./Components/About/About";
 import Campus from "./Components/Campus/Campus";
 import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
 import Hero from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/Navbar";
 import Programs from "./Components/Programs/Programs";
@@ -22,6 +23,7 @@ const App = () => {
         <Testimonial></Testimonial>
         <Title title="Contact Us" subtitle="Get in Touch"></Title>
         <Contact></Contact>
+        <Footer></Footer>
       </div>
     </div>
   );
